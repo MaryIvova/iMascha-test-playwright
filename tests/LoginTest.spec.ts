443d5a7d6bba3cedfa84314c178d6715af34d5c6
iMascha <maryzubova@gmail.com>
2025-04-18 22:07:55 +0300

    new test

--- tests/LoginTest.spec.ts
+++ tests/LoginTest.spec.ts
@@ -1,24 +1,46 @@
 import { test, expect } from '@playwright/test';
+import { AsyncLocalStorage } from 'async_hooks';
+import { Page } from 'playwright-core';
+import { Locator } from 'playwright-core';
 
-test('LogInKMS', async ({ page }) => {
-  await page.goto('https://kms-qa-01.lighthouse-cloud.com/');
+//test.describe('KMSLogin', () => {
+//  test.beforeEach(async ({ page }) => {
 
-  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Login' })).toHaveCSS('font-size', '44px');
-  await expect(page.getByRole('heading', { name: 'Login' })).toHaveCSS('font-weight', '111.66');
-  // await expect(page.getByText('text', 'username')); //.click({ button: 'right' });
-  await page.waitForTimeout(3000);
-});
+//  });
 
 test('LogInKMS2', async ({ page }) => {
   await page.goto('https://kms-qa-08.lighthouse-cloud.com/kms/lh/login');
-
-  await page.getByText('Username', { exact: true }).click();
-  await page.getByRole('textbox', { name: 'Username' }).fill('cm');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('Lighthouse@cm123');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByRole('button', { name: 'Manager' }).click();
-  await page.getByRole('listbox').getByRole('option', { name: 'Content Manager' }).click();
-  await page.getByRole('button', { name: 'Login' }).click();
+  const title = page.locator('//*[@id="kms-login-header"]');
+  expect(page.locator('//*[@id="kms-login-header"]')).toBeVisible();
+  await expect(title).toHaveText('Login');
 });
+
+test('EnterUserName', async ({ page }) => {
+  await page.goto('https://kms-qa-08.lighthouse-cloud.com/kms/lh/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  const username = page.locator('//*[@id="login-username"]');
+  const password = page.locator('//*[@id="login-password"]');
+  const loginButton = page.locator('//*[@id="kms-login-button"]');
+  await username.click();
+  await username.fill('cm');
+  await username.press('Enter');
+  expect(page.locator('//*[@id="login-username"]')).toHaveValue('cm');
+  await password.click();
+  await password.fill('Lighthouse@cm123');
+  await loginButton.click();
+  await expect(page).toHaveURL('https://kms-qa-08.lighthouse-cloud.com/kms/lh/');
+  await expect(page.locator('//*[@id="kms-choose-layout-header"]')).toBeVisible();
+  const dropDown = page.locator('//*[@id="kms-menu-manager"]');
+});
+// await page.getByRole('textbox', { name: 'Username' }).fill('cm');
+// await page.getByRole('textbox', { name: 'Password' }).click();
+// await page.getByRole('textbox', { name: 'Password' }).fill('Lighthouse@cm123');
+// await page.getByRole('button', { name: 'Login' }).click();
+// await page.getByRole('button', { name: 'Manager' }).click();
+// await page.getByRole('listbox').getByRole('option', { name: 'Content Manager' }).click();
+// await page.getByRole('button', { name: 'Login' }).click();
+
+//});
+// test('LayoutSelets', async ({ page }) => {
+// }
+// id = 'login-username';
